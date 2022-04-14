44612dffa16cdf44772df92c71efbce40ad1f176
mooooburg-dev <jeongmupark@gmail.com>
2022-04-14 10:38:06 +0900

    ➕ 의존성 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['media.vlpt.us'],
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
+    //기본 폰트로 사용할 폰트들 기본설정해줌.
+    fontFamily: {
+      sans: ['Noto Sans KR', 'Montserrat'],
+      serif: ['Lato'],
+      mono: ['Corinthia', 'Shadows Into Light', 'Staatliches'],
+      cursive: ['Architects Daughter'],
+      jua: ['Jua'],
+    },
     extend: {},
   },
   plugins: [],
