eddb327afc8bae90ef215f5b35716e36d5931a26
mooooburg-dev <jeongmupark@gmail.com>
2022-04-18 23:15:20 +0900

    ➕ 의존성 설정

--- components/Bottom/index.tsx
+++ components/Bottom/index.tsx
@@ -4,17 +4,25 @@ import RestoreIcon from '@mui/icons-material/Restore';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Link from 'next/link';
+import styles from '../../styles/Bottom.module.scss';
+import { style } from '@mui/system';
 
 export default function Footer() {
   return (
     <Box>
-      <BottomNavigation>
+      <BottomNavigation showLabels>
+        {/*  */}
         <Link href="/profile" passHref>
-          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />}>
+            <a />
+          </BottomNavigationAction>
         </Link>
+        {/* </Link> */}
 
         <Link href={'/messages'} passHref>
-          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <div className="child:opacity-1">
+            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          </div>
         </Link>
 
         <Link href={'/post'} passHref>
